604c95b1c9764bfdcd34521f1c4a2d07d0eae69e
sodaop <saiphirun_p@cmu.ac.th>
2023-05-31 12:52:31 +0700

    update compact function

--- src/views/CompactAssessmentView.vue
+++ src/views/CompactAssessmentView.vue
@@ -1,33 +1,57 @@
 <template>
-  <div class="compact_as">
-    <h1>Compact Home</h1>
-  </div>
   <div>
     <label for="dropdown">Select number of days:</label>
-    <select
-      id="dropdown"
-      v-model="selectedDays"
-      @change="generateAdditionalInputs"
-    >
+    <select id="dropdown" v-model="selectedDays">
       <option value="">Select</option>
       <option v-for="day in days" :value="day" :key="day">{{ day }}</option>
     </select>
 
-    <div v-for="inputIndex in additionalInputsCount" :key="inputIndex">
-      <label :for="'additionalInput' + inputIndex"
-        >Enter date that you been out side:</label
-      >
-      <input
-        :id="'additionalInput' + inputIndex"
-        type="number"
-        v-model="additionalDays[inputIndex - 1]"
-      />
-    </div>
-
-    <button @click="submitData">Submit</button>
+    <label for="averageHours">Average Hours Outside:</label>
+    <input type="number" id="averageHours" v-model="averageHours" required />
   </div>
+  <label for="location1">Select Location 1:</label>
+  <select id="location1" v-model="selectedLocations[0]">
+    <option value="">-- Select Location --</option>
+    <option
+      v-for="(location, index) in locations"
+      :key="index"
+      :value="location"
+    >
+      {{ location }}
+    </option>
+  </select>
+
+  <label for="location2">Select Location 2:</label>
+  <select id="location2" v-model="selectedLocations[1]">
+    <option value="">-- Select Location --</option>
+    <option
+      v-for="(location, index) in locations"
+      :key="index"
+      :value="location"
+    >
+      {{ location }}
+    </option>
+  </select>
+
+  <label for="location3">Select Location 3:</label>
+  <select id="location3" v-model="selectedLocations[2]">
+    <option value="">-- Select Location --</option>
+    <option
+      v-for="(location, index) in locations"
+      :key="index"
+      :value="location"
+    >
+      {{ location }}
+    </option>
+  </select>
+  <button @click="submitData">Submit</button>
+
   <pre>{{ selectedDays }}</pre>
-  <pre>{{ additionalDays }}</pre>
+  <pre>{{ averageHours }}</pre>
+  <pre>{{ selectedLocations }}</pre>
+  <p v-if="responseReceived">averageAQI:{{ calculateaverage }}</p>
+  <p v-if="responseReceived">totalhour:{{ totalhour }}</p>
+  <p v-if="responseReceived">chatGPT suggestion{{ output_text }}</p>
 </template>
 
 <script>
@@ -35,32 +59,48 @@ export default {
   data() {
     return {
       selectedDays: "",
-      additionalDays: [],
-      days: Array.from({ length: 30 }, (_, index) => index + 1), // Generate an array of numbers from 1 to 30
+      calculateaverage: null,
+      totalhour: null,
+      output_text: null,
+      responseReceived: false,
+      averageHours: null,
+      selectedLocations: ["", "", ""],
+      days: Array.from({ length: 30 }, (_, index) => index + 1),
+      locations: [
+        "Innovative Village ต.ป่าแดด อ.เมือง จ.เชียงใหม่",
+        "คณะบริหารธุรกิจ มช. ต.สุเทพ อ.เมือง จ.เชียงใหม่",
+        "คณะมนุษยศาสตร์ มช. (HB5) ต.สุเทพ อ.เมือง จ.เชียงใหม่",
+        "คณะสังคมศาสตร์ มช. ต.สุเทพ อ.เมือง จ.เชียงใหม่",
+        "ชุมชนล่ามช้าง ต.ช้างม่อย อ.เมือง จ.เชียงใหม่ จุดที่ 1 (Indoor)",
+        "ชุมชนวัดเกต ต.วัดเกต อ.เมือง จ.เชียงใหม่ จุดที่ 1 (Indoor)",
+        "ชุมชนสวนดอก ต.สุเทพ อ.เมือง จ.เชียงใหม่",
+        "ชุมชนหมื่นสาร จุดที่ 1B (อาสา DustBoy, Outdoor) ต.หายยา อ.เมือง จ.เชียงใหม่",
+        "ทต.เมืองนะ ต.เมืองนะ อ.เชียงดาว จ.เชียงใหม่",
+        "บ.พะอัน ต.สบโขง อ.อมก๋อย จ.เชียงใหม่",
+        "บ้านดอยสันเกี๋ยง ต.ช่างเคิ่ง อ.แม่แจ่ม จ.เชียงใหม่",
+        "บ้านห้วยบง ต.ทาเหนือ อ.แม่ออน จ.เชียงใหม่",
+        "บ้านห้วยริน ต.ช่างเคิ่ง อ.แม่แจ่ม จ.เชียงใหม่",
+        "มช. แคมปัสแม่เหียะ ต.สุเทพ อ.เมือง จ.เชียงใหม่",
+        "มทร.ล้านนา (ดอยสะเก็ด) ต.ป่าป้อง อ.ดอยสะเก็ด จ.เชียงใหม่",
+        "รพ.ฝาง ต.เวียง อ.ฝาง จ.เชียงใหม่",
+        "รพ.พร้าว ต.เวียง อ.พร้าว จ.เชียงใหม่",
+        "รพ.สต.บ้านไตรสภาวคาม ต.เมืองงาย อ.เชียงดาว จ.เชียงใหม่",
+        "รพ.หางดง ต.หางดง อ.หางดง จ.เชียงใหม่",
+        "สสอ.ฮอด ต.หางดง อ.ฮอด จ.เชียงใหม่",
+        "ศพด.บ้านทับเดื่อ ต.อินทขิล อ.แม่แตง จ.เชียงใหม่",
+      ],
     };
   },
-  computed: {
-    additionalInputsCount() {
-      return this.selectedDays;
-    },
-  },
   methods: {
-    generateAdditionalInputs() {
-      this.additionalDays = [];
-      for (let i = 0; i < this.selectedDays; i++) {
-        this.additionalDays.push(null);
-      }
-    },
     submitData() {
-      // Validate the input before sending the API request
-      if (this.selectedDays !== "" && this.additionalDays !== null) {
+      if (this.numberOfDays !== null && this.averageHours !== null) {
         const requestData = {
+          locations: this.selectedLocations,
           selectedDays: this.selectedDays,
-          additionalDays: this.additionalDays,
+          averageHours: this.averageHours,
         };
 
-        // Send the API request with the collected data
-        fetch("YOUR_API_URL", {
+        fetch("http://127.0.0.1:5000/submitcompactdata", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -69,15 +109,17 @@ export default {
         })
           .then((response) => response.json())
           .then((data) => {
-            // Handle the API response
+            this.calculateaverage = data.calculateaverage;
+            this.totalhour = data.totalhour;
+            this.output_text = data.output_text;
+            this.responseReceived = true;
             console.log(data);
           })
           .catch((error) => {
-            // Handle the error
             console.error("API error:", error);
           });
       } else {
-        alert("Please select the number of days and enter additional days.");
+        alert("Please fill in all fields.");
       }
     },
   },
@@ -86,7 +128,6 @@ export default {
 
 <style scoped>
 label,
-select,
 input {
   margin-bottom: 10px;
 }
