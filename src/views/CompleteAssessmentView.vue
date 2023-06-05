9f218a3c40a99977d2a9652ac6ab3677fc59cb6a
sodaop <saiphirun_p@cmu.ac.th>
2023-06-05 15:16:34 +0700

    compelete additional data

--- src/views/CompleteAssessmentView.vue
+++ src/views/CompleteAssessmentView.vue
@@ -38,6 +38,19 @@
                 type="number"
                 v-model="additionalHours[inputIndex - 1]"
               />
+              <label for="locationDropdown">Select Locations:</label>
+              <select
+                id="locationDropdown"
+                v-model="selectedLocations[inputIndex - 1]"
+              >
+                <option
+                  v-for="location in locations"
+                  :value="location"
+                  :key="location"
+                >
+                  {{ location }}
+                </option>
+              </select>
             </div>
             <label>Enter text:</label>
             <input
@@ -74,6 +87,19 @@
               type="text"
               v-model="additionalText"
             />
+            <label for="locationDropdown">Select Locations:</label>
+            <select
+              id="locationDropdown"
+              v-model="selectedLocations[inputIndex - 1]"
+            >
+              <option
+                v-for="location in locations"
+                :value="location"
+                :key="location"
+              >
+                {{ location }}
+              </option>
+            </select>
           </div>
           <div v-if="selectedDays == 30">
             <label>average hours</label>
@@ -82,7 +108,41 @@
               type="number"
               v-model="Hours"
             />
+            <label for="location1">Select Location 1:</label>
+            <select id="location1" v-model="selectedLocation30[0]">
+              <option value="">-- Select Location --</option>
+              <option
+                v-for="(location, index) in locations"
+                :key="index"
+                :value="location"
+              >
+                {{ location }}
+              </option>
+            </select>
+            <label for="location1">Select Location 2:</label>
+            <select id="location1" v-model="selectedLocation30[1]">
+              <option value="">-- Select Location --</option>
+              <option
+                v-for="(location, index) in locations"
+                :key="index"
+                :value="location"
+              >
+                {{ location }}
+              </option>
+            </select>
+            <label for="location1">Select Location 3:</label>
+            <select id="location1" v-model="selectedLocation30[2]">
+              <option value="">-- Select Location --</option>
+              <option
+                v-for="(location, index) in locations"
+                :key="index"
+                :value="location"
+              >
+                {{ location }}
+              </option>
+            </select>
           </div>
+          <pre>{{ selectedLocation30 }}</pre>
           <button @click="submitData">Submit</button>
         </div>
       </div>
@@ -101,11 +161,36 @@ export default {
       selectedDays: "",
       additionalDays: [],
       additionalHours: [],
+      selectedLocations: [],
+      selectedLocation30: ["", "", ""],
       Hours: null,
       additionalText: null,
       additionalStartDate: null,
       additionalEndDate: null,
       days: Array.from({ length: 30 }, (_, index) => index + 1), // Generate an array of numbers from 1 to 30
+      locations: [
+        "Innovative Village ต.ป่าแดด อ.เมือง จ.เชียงใหม่",
+        "คณะบริหารธุรกิจ มช. ต.สุเทพ อ.เมือง จ.เชียงใหม่",
+        "คณะมนุษยศาสตร์ มช. (HB5) ต.สุเทพ อ.เมือง จ.เชียงใหม่",
+        "คณะสังคมศาสตร์ มช. ต.สุเทพ อ.เมือง จ.เชียงใหม่",
+        "ชุมชนล่ามช้าง ต.ช้างม่อย อ.เมือง จ.เชียงใหม่ จุดที่ 1 (Indoor)",
+        "ชุมชนวัดเกต ต.วัดเกต อ.เมือง จ.เชียงใหม่ จุดที่ 1 (Indoor)",
+        "ชุมชนสวนดอก ต.สุเทพ อ.เมือง จ.เชียงใหม่",
+        "ชุมชนหมื่นสาร จุดที่ 1B (อาสา DustBoy, Outdoor) ต.หายยา อ.เมือง จ.เชียงใหม่",
+        "ทต.เมืองนะ ต.เมืองนะ อ.เชียงดาว จ.เชียงใหม่",
+        "บ.พะอัน ต.สบโขง อ.อมก๋อย จ.เชียงใหม่",
+        "บ้านดอยสันเกี๋ยง ต.ช่างเคิ่ง อ.แม่แจ่ม จ.เชียงใหม่",
+        "บ้านห้วยบง ต.ทาเหนือ อ.แม่ออน จ.เชียงใหม่",
+        "บ้านห้วยริน ต.ช่างเคิ่ง อ.แม่แจ่ม จ.เชียงใหม่",
+        "มช. แคมปัสแม่เหียะ ต.สุเทพ อ.เมือง จ.เชียงใหม่",
+        "มทร.ล้านนา (ดอยสะเก็ด) ต.ป่าป้อง อ.ดอยสะเก็ด จ.เชียงใหม่",
+        "รพ.ฝาง ต.เวียง อ.ฝาง จ.เชียงใหม่",
+        "รพ.พร้าว ต.เวียง อ.พร้าว จ.เชียงใหม่",
+        "รพ.สต.บ้านไตรสภาวคาม ต.เมืองงาย อ.เชียงดาว จ.เชียงใหม่",
+        "รพ.หางดง ต.หางดง อ.หางดง จ.เชียงใหม่",
+        "สสอ.ฮอด ต.หางดง อ.ฮอด จ.เชียงใหม่",
+        "ศพด.บ้านทับเดื่อ ต.อินทขิล อ.แม่แตง จ.เชียงใหม่",
+      ],
     };
   },
   computed: {
@@ -131,8 +216,24 @@ export default {
       }
     },
     submitData() {
-      // Validate the input and submit the data to the API
-      // ...
+      const data = {
+        selectedDays: this.selectedDays,
+        additionalDays: this.additionalDays,
+        additionalStartDate: this.additionalStartDate,
+        additionalEndDate: this.additionalEndDate,
+        additionalHours: this.additionalHours,
+        additionalText: this.additionalText,
+        locations: this.selectedLocations,
+        locations30: this.selectedLocation30,
+        hours30: this.Hours,
+      };
+      fetch("http://127.0.0.1:5000/submitcompletedata", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
     },
   },
 };
