a67759a54c5ab79cbd13b9bfd2d7b364988ba89d
sodaop <saiphirun_p@cmu.ac.th>
2023-06-19 22:39:37 +0700

    commit compact and login unit test

---
+++ tests/unit/compact.spec.js
@@ -0,0 +1,67 @@
+import { mount } from '@vue/test-utils';
+import YourComponent from '@/views/CompactAssessmentView.vue';
+
+describe('YourComponent', () => {
+    let wrapper;
+  
+    beforeEach(() => {
+      wrapper = mount(YourComponent);
+    });
+  
+  
+    it('should set calculateaverage and totalhour correctly when data is submitted', async () => {
+      // Set the necessary data in the component
+      wrapper.setData({
+        selectedDays: 10,
+        averageHours: 8,
+        selectedLocations: ['Location 1', 'Location 2', 'Location 3'],
+      });
+  
+      // Mock the fetch function
+      global.fetch = jest.fn().mockResolvedValue({
+        json: () => ({
+          calculateaverage: 10,
+          totalhour: 100,
+          output_text: 'Some output text',
+        }),
+      });
+  
+      // Trigger the submitData method
+      await wrapper.vm.submitData();
+  
+      // Assertions
+      await wrapper.vm.$nextTick();
+      expect(wrapper.vm.calculateaverage).toBe(10);
+      expect(wrapper.vm.totalhour).toBe(100);
+      expect(wrapper.vm.output_text).toBe('Some output text');
+      expect(wrapper.vm.responseReceived).toBe(true);
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(fetch).toHaveBeenCalledWith('http://127.0.0.1:5000/submitcompactdata', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          locations: ['Location 1', 'Location 2', 'Location 3'],
+          selectedDays: 10,
+          averageHours: 8,
+        }),
+      });
+    });
+  
+    it('should show an error message if fields are not filled', async () => {
+      // Set some fields to empty values
+      wrapper.setData({
+        selectedDays: '',
+        averageHours: null,
+      });
+  
+      // Trigger the submitData method
+      await wrapper.vm.submitData();
+  
+      // Assertions
+      expect(wrapper.vm.calculateaverage).toBeNull();
+      expect(wrapper.vm.totalhour).toBeNull();
+      expect(wrapper.vm.output_text).toBeNull();
+    });
+  });
---
+++ tests/unit/login.spec.js
@@ -0,0 +1,56 @@
+import { shallowMount } from "@vue/test-utils";
+import LoginForm from "@/views/LoginView.vue";
+import { createStore } from 'vuex'
+import 'isomorphic-fetch';
+
+window.alert = jest.fn();
+const store =createStore({
+    state: { loggedIn: false },
+    getters: {},
+    mutations: {
+      SET_LOGGED_IN(state, value) {
+        state.loggedIn = value; // mutation to update loggedIn state
+      },
+    },
+    actions: {
+      setLoggedIn({ commit }, value) {
+        commit("SET_LOGGED_IN", value); // action to trigger mutation
+      },
+    },
+    modules: {},
+  });
+  
+
+describe("LoginForm", () => {
+  it("should submit login details and redirect to home page on successful login", async () => {
+    const $route = {
+        name: "/"
+      }
+    const wrapper = shallowMount(LoginForm, {
+        mocks: {
+          $route
+        }
+      })
+    wrapper.setData({
+      username: "testuser",
+      password: "testpassword",
+    });
+    wrapper.find(".submit-button").trigger("click");
+    store.state.loggedIn = true
+    await wrapper.vm.$nextTick();
+    expect(store.state.loggedIn).toBe(true);
+    expect($route.name).toBe($route.name);
+  });
+
+  it("should show error alert on unsuccessful login", async () => {
+    const wrapper = shallowMount(LoginForm);
+    wrapper.setData({
+      username: "testuser",
+      password: "invalidpassword",
+    });
+    const form = wrapper.find('form');
+    await form.trigger('submit.prevent');
+    await wrapper.vm.$nextTick();
+    expect(wrapper.find('.error-message').exists()).toBe(false);
+  });
+});
