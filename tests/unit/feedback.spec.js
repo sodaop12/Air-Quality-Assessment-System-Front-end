c9cc0a5e33ebfc71a1b75666d2dd7cd8af39f56e
sodaop <saiphirun_p@cmu.ac.th>
2023-08-17 11:15:49 +0700

    added test for feedback feature

---
+++ tests/unit/feedback.spec.js
@@ -0,0 +1,52 @@
+import { mount } from '@vue/test-utils';
+import Footer from '@/components/FeedbackComponent.vue';
+
+describe('Footer.vue', () => {
+  it('submits feedback when the submit button is clicked', async () => {
+    const mockData = { message: 'Feedback submitted successfully' };
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve(mockData),
+    });
+
+    const wrapper = mount(Footer);
+
+    // Set the feedback textarea value
+    const feedbackTextarea = wrapper.find('textarea');
+    await feedbackTextarea.setValue('Testing feedback');
+
+    // Trigger the click event on the submit button
+    await wrapper.find('button').trigger('click');
+
+    // Wait for the Vue component to update
+    await wrapper.vm.$nextTick();
+
+    // Assertions
+    expect(global.fetch).toHaveBeenCalledWith(
+      'http://127.0.0.1:5000/submit_feedback',
+      expect.objectContaining({
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ feedback: 'Testing feedback' }),
+      })
+    );
+
+    expect(wrapper.vm.feedback).toBe('');
+    expect(wrapper.vm.$data.feedback).toBe('');
+
+    // You can also assert the alert message if you've mocked the window.alert function.
+    // For example: expect(window.alert).toHaveBeenCalledWith('Thank you for your feedback!');
+  });
+
+  it('shows an alert when submitting empty feedback', async () => {
+    const mockAlert = jest.spyOn(window, 'alert');
+    const wrapper = mount(Footer);
+
+    await wrapper.find('button').trigger('click');
+    expect(mockAlert).toHaveBeenCalledWith('Please provide feedback before submitting.');
+    mockAlert.mockRestore();
+  });
+
+  // You can write more tests here for different scenarios
+});
