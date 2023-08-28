e375b96d032094c5f33caf19ae2f6824ca60575d
Natthapong_Pop <popbobkok34@hotmail.com>
2023-08-29 05:24:55 +0700

    Update basic view all feedback

--- src/views/AllFeedbackView.vue
+++ src/views/AllFeedbackView.vue
@@ -1,70 +1,61 @@
 <template>
   <div>
-    <div class="container">
-      <div class="left-sidebar">
-        <!-- Left sidebar content -->
-      </div>
-      <div class="content">
-        <!-- Main content -->
-        <div class="forecast">
-          <h1>All Feedback</h1>
-          <ul>
-            <li v-for="(feedback, index) in getFetchedFeedback" :key="index">
-              {{ feedback }}
-            </li>
-          </ul>
+    <h1>Feedback</h1>
+    <div v-for="(feedbackItem, index) in feedback" :key="index">
+      <div class="out-box">
+        <div class="card-set">
+          <img :src="getImagePath(feedbackItem.rating)" alt="Rating Image" />
+          <p>Rating: {{ feedbackItem.rating }}</p>
+          <p>Comment: {{ feedbackItem.feedback }}</p>
         </div>
-      </div>
-      <div class="right-sidebar">
-        <!-- Right sidebar content -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
-
 export default {
-  computed: {
-    ...mapGetters(["getFetchedFeedback"]),
+  data() {
+    return {
+      feedback: [],
+    };
+  },
+  mounted() {
+    this.fetchFeedback();
   },
   methods: {
-    ...mapActions(["fetchFeedback"]), // Map the fetchFeedback action
-  },
-  created() {
-    this.fetchFeedback(); // Call fetchFeedback when the component is created
+    async fetchFeedback() {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/get_feedback");
+        const data = await response.json();
+
+        this.feedback = data.feedback; // Assign the fetched data to the component data
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching feedback:", error);
+      }
+    },
+    getImagePath(rating) {
+      return require(`@/assets/images/star${rating}.jpg`); // Assuming images are named star1.png, star2.png, etc.
+    },
   },
 };
 </script>
 
-<style scoped>
-.container {
-  display: flex;
-  height: 100vh;
-}
-.left-sidebar {
-  width: 25%;
-  background-color: #71ffe5; /* Background color for left sidebar */
+<style scope>
+.feedback-card {
+  margin-bottom: 10px;
 }
 
-.content {
-  width: 50%;
-  background-color: #eafffc; /* Background color for main content */
+.card-set {
+  background-color: #f0f0f0; /* Add your desired background color here */
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 10px;
 }
 
-.right-sidebar {
-  width: 25%;
-  background-color: #71ffe5; /* Background color for right sidebar */
-}
-
-@media (max-width: 768px) {
-  .container {
-    flex-direction: column;
-  }
-
-  .content {
-    width: 100%;
-  }
+.out-box {
+  margin-bottom: 2%;
 }
 </style>
