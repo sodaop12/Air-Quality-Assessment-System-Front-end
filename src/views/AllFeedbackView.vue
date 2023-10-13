cf5fa33d70b03bd026eaf7d95866a8175537ed7c
Natthapong_Pop <popbobkok34@hotmail.com>
2023-10-13 17:11:00 +0700

    All Feedback: improved UI as v-table

--- src/views/AllFeedbackView.vue
+++ src/views/AllFeedbackView.vue
@@ -6,25 +6,31 @@
       </div>
       <div class="content">
         <!-- Main content -->
-        <h1 class="section-title">Feedback</h1>
-        <div class="feedback-cards">
-          <div
-            v-for="(feedbackItem, index) in feedback"
-            :key="index"
-            class="feedback-card"
-          >
-            <div class="card-set">
-              <div class="image-container">
-                <img
-                  :src="getImagePath(feedbackItem.rating)"
-                  alt="Rating Image"
-                  class="rating-image"
-                />
-              </div>
-              <p class="feedback-text">{{ feedbackItem.feedback }}</p>
-            </div>
-          </div>
-        </div>
+        <h2 class="section-title">Feedback</h2>
+        <v-table class="big-table">
+          <thead>
+            <tr>
+              <th class="text-left table-header">Rating</th>
+              <th class="text-left table-header">Comment</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="feedbackItem in feedback" :key="feedbackItem.id">
+              <td>
+                <div class="rating-image-container">
+                  <img
+                    :src="getImagePath(feedbackItem.rating)"
+                    alt="Rating Image"
+                    class="rating-image"
+                  />
+                </div>
+              </td>
+              <td>
+                <p class="feedback-text">{{ feedbackItem.feedback }}</p>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
       </div>
       <div class="right-sidebar">
         <!-- Right sidebar content -->
@@ -66,61 +72,41 @@ export default {
 @import "../assets/css/Based_Element.css";
 @import "../assets/css/Action_Element.css";
 @import "../assets/css/custom_main_font.css";
-.feedback-card {
-  margin-bottom: 10px;
+
+.v-data-table {
+  background-color: transparent; /* Adjust background color as needed */
 }
 
-.card-set {
-  padding: 10px;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  overflow: hidden; /* Add this property */
-  word-wrap: break-word; /* Add this property */
-  width: 647px;
-  height: auto;
-  background-color: #c1fff7;
+.rating-header {
+  font-weight: bold; /* Set font weight for table headers */
+  font-size: 24px; /* Adjust font size for table headers */
+  color: #00926f; /* Set color for table headers */
+  text-align: center; /* Center header text */
 }
 
-.out-box {
-  margin-bottom: 2%;
+.table-header {
+  font-weight: 800; /* Set font weight for table headers */
+  font-size: 25px; /* Adjust font size for table headers */
+  color: #00926f; /* Set color for table headers */
 }
 
-.feedback-cards {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-.feedback-card {
-  margin: 10px; /* Add some margin between cards */
-}
-
-.card-set {
-  padding: 10px;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  overflow: hidden;
-  word-wrap: break-word;
-  width: 647px;
-  height: auto;
-  background-color: #c1fff7;
-  text-align: center; /* Center card content */
-}
-
-.image-container {
+.rating-image-container {
   display: flex;
   justify-content: center;
 }
 
 .rating-image {
-  width: 200px; /* Set a fixed width for the image */
+  width: 280; /* Set a fixed width for the image */
   height: 40px;
 }
 
 .feedback-text {
-  text-align: center;
-  margin-top: 20px;
   font-size: 20px; /* Adjust the font size as needed */
-  font-weight: 600; /* Add font-weight as needed, e.g., 'bold' or 'normal' */
+  font-weight: 400; /* Add font-weight as needed, e.g., 'bold' or 'normal' */
+  text-align: center; /* Center feedback text */
+}
+
+.big-table {
+  padding: 20px;
 }
 </style>
